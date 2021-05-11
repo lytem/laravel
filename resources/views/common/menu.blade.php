3e9427d332c3d9162ae4ecb34aac1561d34141c0
Eliane Temgoua <lytem85@gmailcom>
2021-05-12 01:17:42 +0200

    creazione homepage

--- resources/views/common/menu.blade.php
+++ resources/views/common/menu.blade.php
@@ -1,5 +1,7 @@
+
+
 <div>
-    <a href="/">home</a>
+    <a href="/" style="color: rgb(39, 8, 68)">home</a>
     <a href="/products">products</a>
     <a href="/categories">categories</a>
     <a href="/fornitores">fornitori</a>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,10 +1,25 @@
 <html>
 <head>
-    <style></style>
+
+        <style>
+            body{
+                background-color: rgb(153, 147, 147);
+            }
+            h1{
+                color: blue;
+                padding: 20%;
+                text-align: center;
+                font-size: 50px;
+
+            }
+        </style>
+
+</html>
+    </style>
 </head>
 <body>
     @include('common.menu')
-    <h1> MAGAZZINO BRANDAZZA</h1>
+    <h1><u> MAGAZZINO BRANDAZZA</u></h1>
 
 </body>
 </html>
